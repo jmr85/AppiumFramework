4169d38fb9cc15d5a874a385800531a6f09c7da9
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2022-02-23 15:22:43 -0300

    Section 18. (bugfixed)101. Converting Project into TestNG & eliminating hardcoded paths

--- src/test/java/practise/AppiumFramework/Ecommerce_tc_4.java
+++ src/test/java/practise/AppiumFramework/Ecommerce_tc_4.java
@@ -15,17 +15,19 @@ public class Ecommerce_tc_4 extends base {
 	@Test
 	public void totalValidation() throws IOException, InterruptedException {
 		// 15 -General-Store.apk4.0
-		
+
 		AndroidDriver<AndroidElement> driver = capabilities("GeneralStoreApp");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("Hello");
 		driver.hideKeyboard();
 		driver.findElement(By.xpath("//*[@text='Female']")).click();
 		driver.findElement(By.id("android:id/text1")).click();
-		driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Argentina\"));");
+		driver.findElementByAndroidUIAutomator(
+				"new UiScrollable(new UiSelector()).scrollIntoView(text(\"Argentina\"));");
+		// if it doesn't work try this line
 		// driver.findElement(MobileBy.AndroidUIAutomator("new UiScrollable(new
 		// UiSelector().scrollable(true).instance(0)).scrollIntoView(new
-		// UiSelector().textMatches(\"" + containedText + "\").instance(0))"));
+		// UiSelector().textMatches(\"Jordan 6 Rings\").instance(0))"));
 		driver.findElement(By.xpath("//*[@text='Argentina']")).click();
 		driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).click();
 
@@ -36,18 +38,17 @@ public class Ecommerce_tc_4 extends base {
 		int count = driver.findElements(By.id("com.androidsample.generalstore:id/productPrice")).size();
 		double sum = 0;
 		for (int i = 0; i < count; i++) {
-			String amount1 = driver.findElements(By.id("com.androidsample.generalstore:id/productName")).get(i).getText();
+			String amount1 = driver.findElements(By.id("com.androidsample.generalstore:id/productPrice")).get(i)
+					.getText();
 			double amount = getAmount(amount1);
-			sum = sum + amount;// 280.97+116.97.
+			sum += amount;// 280.97
 		}
-		System.out.println(sum + "sum of products");
-
+		System.out.println("sum of products: " + sum);
 		String total = driver.findElement(By.id("com.androidsample.generalstore:id/totalAmountLbl")).getText();
-
 		total = total.substring(1);
-		double totalValue = Double.parseDouble(total);
-		System.out.println(totalValue + "Total value of products");
-		Assert.assertEquals(sum, totalValue);
+		double totalvalue = Double.parseDouble(total);
+		System.out.println("Total value of products: " + totalvalue);
+		Assert.assertEquals(sum, totalvalue);
 
 	}
 
